fb7d90affb99bef025d25157564c6491ee73bc65
Sebastian Markgraf <Sebastian-Markgraf@t-online.de>
2022-10-28 08:20:17 +0200

    Add sorting of glob sources

--- setup.py
+++ setup.py
@@ -8,16 +8,10 @@ from setuptools import setup
 
 __version__ = "0.0.1"
 
-# The main interface is through Pybind11Extension.
-# * You can add cxx_std=11/14/17, and then build_ext can be removed.
-# * You can set include_pybind11=false to add the include directory yourself,
-#   say from a submodule.
-#
-# Note:
 #   Sort input source files if you glob sources to ensure bit-for-bit
 #   reproducible builds (https://github.com/pybind/python_example/pull/53)
 
-ext_modules = intree_extensions(glob("forward_models/*.cpp"))
+ext_modules = intree_extensions(sorted(glob("forward_models/*.cpp")))
 print(ext_modules)
 
 setup(
