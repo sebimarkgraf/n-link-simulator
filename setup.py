b6f7398c8cf465ad2f0226111cd599ba826ab0ac
Sebastian Markgraf <Sebastian-Markgraf@t-online.de>
2022-10-30 15:53:55 +0100

    Fix cmdclass as pybind11 does not support python packages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from pybind11 import get_cmake_dir
 from pybind11.setup_helpers import intree_extensions, build_ext
 from setuptools import setup, find_packages
 
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 
 #   Sort input source files if you glob sources to ensure bit-for-bit
 #   reproducible builds (https://github.com/pybind/python_example/pull/53)
@@ -24,12 +24,9 @@ setup(
     ext_modules=ext_modules,
 
     extras_require={"test": "pytest"},
-    # Currently, build_ext only provides an optional "highest supported C++
-    # level" feature, but in the future it may provide more features.
-    cmdclass={"build_ext": build_ext},
     zip_safe=False,
     python_requires=">=3.6",
     install_requires=["numpy"],
-    packackages=find_packages()
+    packages=find_packages(include=["n_link_sim"])
 )
 
