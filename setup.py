78a97c7f2a1ba7c2a1a76de996bcd4f7c44294ec
Sebastian Markgraf <Sebastian-Markgraf@t-online.de>
2022-10-30 15:35:11 +0100

    Add missing find packages to setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ from glob import glob
 from pybind11 import get_cmake_dir
 # Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import intree_extensions, build_ext
-from setuptools import setup
+from setuptools import setup, find_packages
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 #   Sort input source files if you glob sources to ensure bit-for-bit
 #   reproducible builds (https://github.com/pybind/python_example/pull/53)
 
 ext_modules = intree_extensions(sorted(glob("n_link_sim/*.cpp")))
-print(ext_modules)
 
 setup(
     name="n-link-simulator",
@@ -23,12 +22,14 @@ setup(
     description="Simulators for double and quad link dynamics, taken from pypost toolbox",
     long_description="",
     ext_modules=ext_modules,
+
     extras_require={"test": "pytest"},
     # Currently, build_ext only provides an optional "highest supported C++
     # level" feature, but in the future it may provide more features.
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
     python_requires=">=3.6",
-    install_requires=["numpy"]
+    install_requires=["numpy"],
+    packackages=find_packages()
 )
 
